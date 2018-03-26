cce714fa63f7c576fd44d92d26e8ba23abffeb50
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-26 12:06:04 -0600

    adds new user routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  
+
+  get '/register', to: "users#new"
+
   namespace :api do
     namespace :v1 do
       resources :games, only: [:show] do

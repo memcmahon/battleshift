c548a768cea264a38dd660916b5cd98bb4e11f98
slimecog <wlcjohnson@gmail.com>
2018-03-26 16:05:33 -0600

    adds user-mailer test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "registration email" do
+    it "includes activation link" do
+      user = User.create!(name: "Testy Dude", email: "email@email.com", password: "Test!")
+      mail = UserMailer.registration_email(user)
+
+      expect(mail.subject).to eq("Battleshift Activation Email")
+      expect(mail.to).to eq(["email@email.com"])
+      expect(mail.from).to eq(["thecaptain@battleshift.com"])
+      expect(mail.body.encoded).to include("http://localhost:3000/activate/#{user.id}")
+    end
+  end
 end

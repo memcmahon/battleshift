b72ff67ede972ab89823da718965fe8e711cb249
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-27 10:23:09 -0600

    Adds players to game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327005327) do
+ActiveRecord::Schema.define(version: 20180327162242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20180327005327) do
     t.integer "current_turn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "player_1"
+    t.integer "player_2"
   end
 
   create_table "users", force: :cascade do |t|

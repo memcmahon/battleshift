6b202e19c097940fef46670adbf8b70cc50b6538
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-27 16:50:04 -0600

    Adds has one api key to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ require "digest"
 
 class User < ApplicationRecord
   has_many :api_keys
+  has_one :api_key, -> { where(status: "active")}
   has_one :game
+
 
   after_create :set_activation_key
   after_create :set_api_key
@@ -16,10 +18,10 @@ class User < ApplicationRecord
     sha = Digest::SHA1.hexdigest(email)
     update(activation_key: sha)
   end
-
-  def active_api_key
-    api_keys.find_by(status: "active")
-  end
+  #
+  # def active_api_key
+  #   api_keys.find_by(status: "active")
+  # end
 
   def set_api_key
     api_keys.create!(status: 'active')

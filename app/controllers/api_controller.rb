aa106ff07a28ca5e023eac28cc96af15d65e5fff
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-29 15:08:14 -0600

    Updates filter to check that api key exists and is active

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,7 @@ class ApiController < ActionController::API
 
   private
     def verify_api_key
-      unless ApiKey.find_by(id: request.headers["X-API-KEY"])
+      if ApiKey.find_by(id: request.headers["X-API-KEY"]).nil? || ApiKey.find_by(id: request.headers["X-API-KEY"]).status != "active"
         render json: {message: "Unauthorized"}, status: 401
       end
     end

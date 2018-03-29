259e584f4daa6f6171431c7b3e9e7f9f33349eb6
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-29 14:54:40 -0600

    Adds request spec for invalid api keys

---
+++ spec/requests/user_can_not_make_request_with_invalid_api_key_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "user must use valid api key when making requests" do
+  let(:player_1) { create(:user) }
+  let(:player_2) { create(:user) }
+  let(:payload) { {opponent_email: player_2.email}.to_json }
+
+  it "they can make posts with valid key" do
+    headers = { "CONTENT_TYPE" => "application/json",
+                "X-API-Key" => player_1.api_key.id
+              }
+
+    post "/api/v1/games", params: payload, headers: headers
+
+    expect(response.status).to eq(200)
+  end
+
+  it "they can not make posts with invalid key" do
+    headers = { "CONTENT_TYPE" => "application/json",
+                "X-API-Key" => "invalid key"
+              }
+
+    post "/api/v1/games", params: payload, headers: headers
+
+    results = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response.status).to eq(401)
+    expect(results[:message]).to eq("Unauthorized")
+  end
+end

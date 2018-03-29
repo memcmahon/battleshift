077a942ab05d4b66d36efe3c38604e8582d72335
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-29 15:07:42 -0600

    Adds spec to verify active api key

--- spec/requests/user_can_not_make_request_with_invalid_api_key_spec.rb
+++ spec/requests/user_can_not_make_request_with_invalid_api_key_spec.rb
@@ -4,6 +4,7 @@ describe "user must use valid api key when making requests" do
   let(:player_1) { create(:user) }
   let(:player_2) { create(:user) }
   let(:payload) { {opponent_email: player_2.email}.to_json }
+  let(:inactive_key) { ApiKey.create!(status: "inactive", user: player_2) }
 
   it "they can make posts with valid key" do
     headers = { "CONTENT_TYPE" => "application/json",
@@ -23,7 +24,20 @@ describe "user must use valid api key when making requests" do
     post "/api/v1/games", params: payload, headers: headers
 
     results = JSON.parse(response.body, symbolize_names: true)
-    
+
+    expect(response.status).to eq(401)
+    expect(results[:message]).to eq("Unauthorized")
+  end
+
+  it "they can not make posts with inactive key" do
+    headers = { "CONTENT_TYPE" => "application/json",
+                "X-API-Key" => inactive_key
+              }
+
+    post "/api/v1/games", params: payload, headers: headers
+
+    results = JSON.parse(response.body, symbolize_names: true)
+
     expect(response.status).to eq(401)
     expect(results[:message]).to eq("Unauthorized")
   end

31720c94fae92f42036d3ac7b84dd434e7db35be
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-29 14:55:20 -0600

    Adds before action on api controller to verify api_key exists

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,10 @@
 class ApiController < ActionController::API
+  before_action :verify_api_key
+
+  private
+    def verify_api_key
+      unless ApiKey.find_by(id: request.headers["X-API-KEY"])
+        render json: {message: "Unauthorized"}, status: 401
+      end
+    end
 end

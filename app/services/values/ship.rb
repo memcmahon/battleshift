feb40da5c3b9f5bfd0b71c5464b9f73a2ab7c13b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-29 21:25:48 -0600

    Adds ship tests and updates simplecov setup

--- app/services/values/ship.rb
+++ app/services/values/ship.rb
@@ -1,15 +1,13 @@
 class Ship
-  attr_reader :length, :damage, :start_space,
-              :end_space
+  attr_reader :length, :damage
 
   def initialize(length)
     @length = length
     @damage = 0
-    @start_space = nil
-    @end_space = nil
   end
 
   def attack!
+    binding.pry
     @damage += 1
   end
 

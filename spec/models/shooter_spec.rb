2972a93007d9e9762a8f67ee0a04db44d6f7e58d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-31 13:26:32 -0600

    Adds shooter spec

--- spec/models/shooter_spec.rb
+++ spec/models/shooter_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 describe Shooter, type: :model do
   let(:board) { Board.new(4) }
   let(:target) { "A1" }
+  let(:subject) { Shooter.new(board: board, target: target)}
 
-  xit "can fire a shot" do
-    result = Shooter.fire!(board: board, target: target)
+  it "exists" do
+    expect(subject).to be_instance_of Shooter
+  end
 
-    binding.pry
+  it "can fire a shot" do
+    result = subject.fire!
+
+    expect(result).to eq("Miss")
   end
 end

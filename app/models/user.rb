15091c282fad1ab58006caeb5e456abbde269589
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-31 12:16:14 -0600

    Adds model method specs to user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates_presence_of :email, :name#, :password, :password_confirmation
+  validates_presence_of :email, :name
   validates_uniqueness_of :email
-  # validates_confirmation_of :password
 
   def set_activation_key
     sha = Digest::SHA1.hexdigest(email)

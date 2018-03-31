047b5e3c77b4843af32372222821a0545af8f752
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-31 12:07:20 -0600

    Adds apikey factory

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,13 +3,11 @@ require "rails_helper"
 RSpec.describe UserMailer, type: :mailer do
   describe "registration email" do
     it "includes activation link" do
-      user = double('user')
-      allow(user).to receive(:activation_key) { 'fakeactivationkey' }
-      allow(user).to receive(:api_key) { create(:api_key) }
+      user = create(:user)
       mail = UserMailer.registration_email(user)
 
       expect(mail.subject).to eq("Battleshift Activation Email")
-      expect(mail.to).to eq(["email@email.com"])
+      expect(mail.to).to eq(["#{user.email}"])
       expect(mail.from).to eq(["thecaptain@battleshift.com"])
       expect(mail.body.encoded).to include("http://localhost:3000/activate/#{user.activation_key}")
       expect(mail.body.encoded).to include(user.api_key.id)

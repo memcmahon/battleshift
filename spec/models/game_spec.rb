ebaeb82c9c83af333b44d144b1ff05f1d022f72a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-31 12:10:21 -0600

    adds validation tests to game spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Game, type: :model do
   describe "Relationships" do
     it { should belong_to :player_1 }
     it { should belong_to :player_2 }
+    it { should validate_presence_of :player_1_board }
+    it { should validate_presence_of :player_2_board }
   end
 end

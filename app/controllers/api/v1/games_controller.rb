7e971e50edd80c08c406f15c977300e9c2d7195e
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-01 10:57:39 -0600

    Refactors game controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -11,10 +11,7 @@ class Api::V1::GamesController < ApiController
   end
 
   def create
-    player_1 = User.find(ApiKey.find(request.headers["X-API-Key"]).user_id)
-    player_2 = User.find_by(email: params[:opponent_email])
-
-    game = Game.new(player_1: player_1, player_2: player_2,
+    game = Game.new(player_1: challenger, player_2: opponent,
                     player_1_board: Board.new(4), player_2_board: Board.new(4))
 
     game.save
@@ -27,6 +24,10 @@ class Api::V1::GamesController < ApiController
       unless opponent && opponent.activated
         render json: {message: "The opponent you have chosen is not an active user."}, status: 400
       end
+    end
+
+    def challenger
+      User.find(ApiKey.find(request.headers["X-API-Key"]).user_id)
     end
 
     def opponent

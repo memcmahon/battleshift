0d5903ed64254e7e1d759a51657acd6d78e4867d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-04-01 10:52:16 -0600

    Adds check for opponent being user

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -24,8 +24,12 @@ class Api::V1::GamesController < ApiController
 
   private
     def verify_opponent
-      unless User.find_by(email: params[:opponent_email]).activated
+      unless opponent && opponent.activated
         render json: {message: "The opponent you have chosen is not an active user."}, status: 400
       end
+    end
+
+    def opponent
+      User.find_by(email: params[:opponent_email])
     end
 end
